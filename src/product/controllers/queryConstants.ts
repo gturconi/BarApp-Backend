aef0a4bf7ba933bca6376e24eb06f1571ad47c01
franGiangiordano <frangiangiordano@gmail.com>
2024-01-08 09:11:25 -0300

    add product id to queries

--- src/product/controllers/queryConstants.ts
+++ src/product/controllers/queryConstants.ts
@@ -1,5 +1,5 @@
 export const SELECT_PRODUCT_BY_ID =
-  "SELECT p.name, p.description, p.image, c.description AS category, p.baja, p.stock, pre.price FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE pre.valid_from = ( SELECT MAX(valid_from) FROM productPrice WHERE idProd = p.id ) AND p.id = ?";
+  "SELECT p.id, p.name, p.description, p.image, c.description AS category, p.baja, p.stock, pre.price FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE pre.valid_from = ( SELECT MAX(valid_from) FROM productPrice WHERE idProd = p.id ) AND p.id = ?";
 
 export const SELECT_PRODUCT_BY_NAME = "SELECT * FROM products WHERE name = ?";
 
@@ -10,10 +10,10 @@ export const COUNT_PRODUCTS_BY_TYPE =
   "SELECT COUNT(*) AS total FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE p.idCat = ? AND (p.name LIKE CONCAT('%', ?, '%') OR p.description LIKE CONCAT('%', ?, '%') OR c.description LIKE CONCAT('%', ?, '%') OR pre.price < ?);";
 
 export const SELECT_PRODUCTS =
-  "SELECT p.name, p.description, p.image, c.description AS category, p.baja, p.stock, pre.price as price FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE pre.valid_from = ( SELECT MAX(valid_from) FROM productPrice WHERE idProd = p.id ) AND (p.name LIKE CONCAT('%', ?, '%') OR p.description LIKE CONCAT('%', ?, '%') OR c.description LIKE CONCAT('%', ?, '%') OR pre.price < ?) ORDER BY p.name ASC LIMIT ?, ?";
+  "SELECT p.id, p.name, p.description, p.image, c.description AS category, p.baja, p.stock, pre.price as price FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE pre.valid_from = ( SELECT MAX(valid_from) FROM productPrice WHERE idProd = p.id ) AND (p.name LIKE CONCAT('%', ?, '%') OR p.description LIKE CONCAT('%', ?, '%') OR c.description LIKE CONCAT('%', ?, '%') OR pre.price < ?) ORDER BY p.name ASC LIMIT ?, ?";
 
 export const SELECT_PRODUCTS_BY_TYPE =
-  "SELECT p.name, p.description, p.image, c.description AS category, p.baja, p.stock, pre.price as price FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE pre.valid_from = ( SELECT MAX(valid_from) FROM productPrice WHERE idProd = p.id ) AND p.idCat = ? AND (p.name LIKE CONCAT('%', ?, '%') OR p.description LIKE CONCAT('%', ?, '%') OR c.description LIKE CONCAT('%', ?, '%') OR pre.price < ?) ORDER BY p.name ASC LIMIT ?, ?;";
+  "SELECT p.id, p.name, p.description, p.image, c.description AS category, p.baja, p.stock, pre.price as price FROM products AS p INNER JOIN productsType AS c ON p.idCat = c.idProductType INNER JOIN productPrice AS pre ON pre.idProd = p.id WHERE pre.valid_from = ( SELECT MAX(valid_from) FROM productPrice WHERE idProd = p.id ) AND p.idCat = ? AND (p.name LIKE CONCAT('%', ?, '%') OR p.description LIKE CONCAT('%', ?, '%') OR c.description LIKE CONCAT('%', ?, '%') OR pre.price < ?) ORDER BY p.name ASC LIMIT ?, ?;";
 
 export const INSERT_PRODUCT =
   "INSERT INTO products (name, description, image, idCat, stock) VALUES (?, ?, ?, ?, ?);";
