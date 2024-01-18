1bc33f58133118af7c9751654fe176554996b5dc
gturconi <35380017+gturconi@users.noreply.github.com>
2024-01-18 14:50:01 -0300

    Id ProductType Changed

--- src/product-type/controllers/queryConstants.ts
+++ src/product-type/controllers/queryConstants.ts
@@ -1,20 +1,19 @@
 export const SELECT_PRODUCT_TYPE_BY_ID =
-  "SELECT idProductType, description, image FROM productsType WHERE idProductType = ?";
+  'SELECT id, description, image FROM productsType WHERE id = ?';
 
 export const COUNT_PRODUCTS_TYPE =
   "SELECT COUNT(*) AS total FROM productsType WHERE description LIKE CONCAT('%', ?, '%');";
 
 export const SELECT_PRODUCTS_TYPE =
-  "SELECT idProductType, description, image FROM productsType WHERE description LIKE CONCAT('%', ?, '%') ORDER BY description ASC LIMIT ?, ?";
+  "SELECT id, description, image FROM productsType WHERE description LIKE CONCAT('%', ?, '%') ORDER BY description ASC LIMIT ?, ?";
 
 export const UPDATE_PRODUCT_TYPE =
-  "UPDATE productsType SET description = IFNULL(?, description), image = IFNULL(?, image) WHERE idProductType = ?";
+  'UPDATE productsType SET description = IFNULL(?, description), image = IFNULL(?, image) WHERE id = ?';
 
-export const DELETE_PRODUCT_TYPE =
-  "DELETE FROM productsType WHERE idProductType = ?";
+export const DELETE_PRODUCT_TYPE = 'DELETE FROM productsType WHERE id = ?';
 
 export const SELECT_PRODUCT_TYPE_BY_DESCRIPTION =
-  "SELECT * FROM productsType WHERE description = ?";
+  'SELECT * FROM productsType WHERE description = ?';
 
 export const INSERT_PRODUCT_TYPE =
-  "INSERT INTO productsType (description, image) VALUES (?, ?)";
+  'INSERT INTO productsType (description, image) VALUES (?, ?)';
