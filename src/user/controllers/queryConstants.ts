51e0dc4898db29be9aa15166e46c0043dc5a1338
gturconi <35380017+gturconi@users.noreply.github.com>
2024-03-13 13:31:55 -0300

    Update queryConstants.ts

--- src/user/controllers/queryConstants.ts
+++ src/user/controllers/queryConstants.ts
@@ -5,7 +5,7 @@ export const SELECT_ROLE_BY_NAME = 'SELECT * FROM roles WHERE name = ?';
 export const SELECT_USER_BY_ID_NO_JOIN = 'SELECT * FROM users WHERE id = ?';
 
 export const SELECT_USER_BY_ID =
-  'SELECT u.id, u.name, u.tel, u.email, r.name AS role, u.avatar FROM users u INNER JOIN roles AS r ON u.rol_id = r.id WHERE u.id = ?';
+  'SELECT u.id, u.name, u.tel, u.email, r.name AS role, u.avatar, u.baja FROM users u INNER JOIN roles AS r ON u.rol_id = r.id WHERE u.id = ?';
 
 export const GET_USER_PASSWORD = 'SELECT password FROM users WHERE id = ?';
 
@@ -16,7 +16,7 @@ export const COUNT_USERS =
   "SELECT COUNT(*) AS total FROM users AS us INNER JOIN roles AS rol ON us.rol_id = rol.id WHERE us.name LIKE CONCAT('%', ?, '%') OR us.tel LIKE CONCAT('%', ?, '%') OR us.email LIKE CONCAT('%', ?, '%') OR rol.name LIKE CONCAT('%', ?, '%');";
 
 export const SELECT_USERS =
-  "SELECT u.id, u.name, u.tel, u.email, r.name AS role, u.avatar FROM users AS u INNER JOIN roles AS r ON u.rol_id = r.id WHERE u.name LIKE CONCAT('%', ?, '%') OR u.tel LIKE CONCAT('%', ?, '%') OR u.email LIKE CONCAT('%', ?, '%') OR r.name LIKE CONCAT('%', ?, '%') ORDER BY u.name ASC LIMIT ?, ?";
+  "SELECT u.id, u.name, u.tel, u.email, r.name AS role, u.avatar, u.baja FROM users AS u INNER JOIN roles AS r ON u.rol_id = r.id WHERE u.name LIKE CONCAT('%', ?, '%') OR u.tel LIKE CONCAT('%', ?, '%') OR u.email LIKE CONCAT('%', ?, '%') OR r.name LIKE CONCAT('%', ?, '%') ORDER BY u.name ASC LIMIT ?, ?";
 
 export const UPDATE_USER =
   'UPDATE users SET name = IFNULL(?, name), tel = IFNULL(?, tel), email = IFNULL(?, email), password = IFNULL(?, password) ,rol_id = IFNULL(?, rol_id), baja = IFNULL(?, baja) ,avatar = IFNULL(?, avatar) WHERE id = ?';
