bdcc831073bec620a3d0912d8b96c02a7fea189a
gturconi <35380017+gturconi@users.noreply.github.com>
2024-04-12 15:37:02 -0300

    Update body limit size

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import routes from './routes';
 const app = express();
 
 // Middlewares
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
