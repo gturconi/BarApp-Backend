63bde8271e2531131de9cf8729c1f99c3bb0c9ad
gturconi <35380017+gturconi@users.noreply.github.com>
2024-04-20 20:10:06 -0300

    Update queryConstants.ts

--- src/user/controllers/queryConstants.ts
+++ src/user/controllers/queryConstants.ts
@@ -30,4 +30,4 @@ export const SELECT_FCM_TOKEN =
   'SELECT fcm_token FROM users WHERE user.baja = 0';
 
 export const SELECT_EMPLOYEE_FCM_TOKEN =
-  'SELECT us.fcm_token FROM users AS us INNER JOIN roles AS rol ON us.rol_id = rol.id WHERE rol.name LIKE "employee" AND us.baja = 0';
+  'SELECT us.fcm_token FROM users AS us INNER JOIN roles AS rol ON us.rol_id = rol.id WHERE rol.name LIKE "employee" AND us.baja = 0 AND us.fcm_token IS NOT NULL';
