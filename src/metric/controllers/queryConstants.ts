bd9c1384751b1753182553a4f0c6d9035599064a
gturconi <35380017+gturconi@users.noreply.github.com>
2024-04-28 19:11:11 -0300

    Limit 5 products

--- src/metric/controllers/queryConstants.ts
+++ src/metric/controllers/queryConstants.ts
@@ -1,2 +1,2 @@
 export const MOST_SELLED_PRODUCTS =
-  'SELECT p.name, COUNT(*) as cant FROM products p LEFT JOIN orderDetail od ON p.id = od.productId LEFT JOIN orders o ON od.orderId = o.id  AND o.date_created >= DATE_FORMAT(CURDATE(), "%Y-%m-01") LEFT JOIN orderState os ON o.idState = os.id AND os.description = "Pagado" GROUP BY p.name ORDER BY cant LIMIT 10';
+  'SELECT p.name, COUNT(*) as cant FROM products p LEFT JOIN orderDetail od ON p.id = od.productId LEFT JOIN orders o ON od.orderId = o.id  AND o.date_created >= DATE_FORMAT(CURDATE(), "%Y-%m-01") LEFT JOIN orderState os ON o.idState = os.id AND os.description = "Pagado" GROUP BY p.name ORDER BY cant LIMIT 5';
