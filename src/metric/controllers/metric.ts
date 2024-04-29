4b6beb4b46465469bba4a79187a0204e6f634bfb
gturconi <35380017+gturconi@users.noreply.github.com>
2024-04-29 19:53:47 -0300

    Top Customers

--- src/metric/controllers/metric.ts
+++ src/metric/controllers/metric.ts
@@ -7,7 +7,7 @@ import { handleServerError } from '../../shared/errorHandler';
 import { EntityListResponse } from '../../shared/models/entity.list.response.model';
 
 import * as queryConstants from './queryConstants';
-import { ProductSelled } from '../models/metrics';
+import { ProductSelled, TopCustomers } from '../models/metrics';
 
 export const getMostSelledProdcuts = async (req: Request, res: Response) => {
   try {
@@ -18,6 +18,27 @@ export const getMostSelledProdcuts = async (req: Request, res: Response) => {
     if (metric.length <= 0) {
       return res.status(404).json({
         message: 'No se encontraron productos vendidos',
+      });
+    }
+    return res.status(200).json(metric);
+  } catch (error) {
+    return handleServerError({
+      res,
+      message: 'Ocurrio un error al obtener la metrica',
+      errorNumber: 500,
+    });
+  }
+};
+
+export const topFiveCustomers = async (req: Request, res: Response) => {
+  try {
+    const [metric] = await pool.query<DbQueryResult<TopCustomers[]>>(
+      queryConstants.TOP_FIVE_CUSTOMERS
+    );
+
+    if (metric.length <= 0) {
+      return res.status(404).json({
+        message: 'No se encontraron pedidos',
       });
     }
     return res.status(200).json(metric);
--- src/routes/metric.ts
+++ src/routes/metric.ts
@@ -2,7 +2,10 @@ import express from 'express';
 
 import { isAdmin, verifyToken } from '../middlewares/authJwt';
 
-import { getMostSelledProdcuts } from '../metric/controllers/metric';
+import {
+  getMostSelledProdcuts,
+  topFiveCustomers,
+} from '../metric/controllers/metric';
 
 const router = express.Router();
 
@@ -11,5 +14,6 @@ router.get(
   [verifyToken, isAdmin],
   getMostSelledProdcuts
 );
+router.get('/top-five-customers', [verifyToken, isAdmin], topFiveCustomers);
 
 export default router;
